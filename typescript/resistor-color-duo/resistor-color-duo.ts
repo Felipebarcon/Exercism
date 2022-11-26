246042340a0792220bfc09a8bd22fc0f60ab2439
Felipe Barcon <felipe.barcon@gmail.com>
2022-11-26 19:41:47 +0100

    exercice 3 ok

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,26 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+interface Color {
+  [color: string]: number;
+}
+
+const colorsMap: Color = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export function decodedValue(colors: string[]): number {
+  if (colors.length < 2) {
+    throw "At least two colors need to be present";
+  }
+
+  const [first, second] = colors;
+
+  return Number(`${colorsMap[first]}${colorsMap[second]}`);
 }
