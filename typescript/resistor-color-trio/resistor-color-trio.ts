6135254cf7d22253731e8f70db65b9536d926e7c
Felipe Barcon <felipe.barcon@gmail.com>
2022-11-30 17:38:02 +0100

    initial commit resistor color trio

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,19 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+interface Color {
+  [color: string]: number;
+}
+
+const colorsMap: Color = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+
+export function decodedResistorValue(color: string): number {
+  throw new Error("Remove this statement and implement this function");
 }
